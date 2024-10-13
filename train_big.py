aef3ee57ed894a0aeb71cbc55f427b9e38099207
Jacob Larocca <laroccajacob90@gmail.com>
2024-10-13 16:40:41 -0700

    add shakespeare

--- train_big.py
+++ train_big.py
@@ -1,12 +1,78 @@
 from dataclasses import dataclass
+import math
+import tiktoken
+
 import torch
 import torch.nn as nn
-from torch.nn import functional
+from torch.nn import functional as F
+
+class CausalSelfAttention(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # mask on the outputs to make it backward-looking only.
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+        
+    def forward(self, x):
+        B, T, C = x.size() # batch size, seq length, embedding dimension
+        # calculate query, key, value for all heads in batch.
+        # nh = num heads, hs = head size, C = nh * hs = channels
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # attention (materializes (T, T) matrix for all queries/keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x += self.attn(self.ln_1(x))
+        x += self.mlp(self.ln_2(x))
+        return x
 
 @dataclass
 class GPTConfig:
     block_size: int = 256
-    vocab_size: int = 65
+    vocab_size: int = 1256
     n_layer: int = 6
     n_head: int = 6
     n_embd: int = 384
@@ -18,9 +84,73 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd)
-            wpe = nn.Embedding(config.block_size, config.n_embd)
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+    
+    def forward(self, idx):
+        B, T = idx.size()
+        assert T <= self.config.block_size
+        # forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward through transformer blocks
+        for block in self.transformer.h:
+            x = block(x)
+        # forward through final layer norm
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        return logits
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+print("using device: ", device)
+device_type = "cuda" if device.startswith("cuda") else "cpu"
+max_length = 30
+num_return_sequences = 5
+
+model = GPT(GPTConfig())
+model.eval()
+model.to(device)
+
+enc = tiktoken.get_encoding('gpt2')
+# just get the first 1000 + 256 originals
+enc._mergeable_ranks = dict(list(enc._mergeable_ranks.items())[:1256])
+enc._core_bpe = tiktoken._tiktoken.CoreBPE(enc._mergeable_ranks, enc._special_tokens, enc._pat_str) 
+tokens = enc.encode('I am a language model. ')
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to(device)
+
+# Generation (move to fn)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward model to get logits
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+        # take logits at last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+        # get probabilities
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 10
+        # topk_probs is now (5, 10)
+        topk_probs, topk_indices = torch.topk(probs, 10, dim=-1)
+        # select token from top-k
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        # gather corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to seq
+        x = torch.cat((x, xcol), dim=1)
+
+# print generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
